8483db50b255928518f858212700edeea23f88d8
Broccoli <vovo.r@ya.ru>
2023-12-18 20:41:31 +1000

    edit

--- test/test_envfetch.py
+++ test/test_envfetch.py
@@ -1,5 +1,9 @@
 from unittest.mock import patch
 import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from envfetch import EnvFetch
 
 @pytest.fixture
